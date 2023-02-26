a533b3fb421895b7de446042e23d1273707e321e
Hyunzee <hyunzeekim01@gmail.com>
2023-02-25 20:10:26 -0800

    Sending email test

---
+++ backend/typescript/services/implementations/__tests__/sendEmail.test.ts
@@ -0,0 +1,11 @@
+import EmailService from "../emailService";
+import IEmailService from "../../interfaces/emailService";
+import nodemailerConfig from "../../../nodemailer.config";
+
+const emailService: IEmailService = new EmailService(nodemailerConfig);
+
+describe("sendEmail", () => {
+  it("sendEmail", async () => {
+    await emailService.sendEmail("hyunzeekim@uwblueprint.org", "test", "testing");
+  });
+});
