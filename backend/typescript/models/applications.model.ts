060a1a81fca6b1fd87b288698f7e9312814f31db
tshenqka <maximatron115@Gmail.com>
2023-03-12 01:06:25 -0500

    Initial changes. Added script to update env var at fixed time interval. Adding method to monitor for changes and print to console

---
+++ backend/typescript/models/applications.model.ts
@@ -0,0 +1,38 @@
+import { Column, Model, Table } from 'sequelize-typescript';
+import { ApplicationStatus } from '../types';
+
+@Table({ tableName: 'applications' })
+export default class Application extends Model {
+  @Column({ type: DataType.STRING })
+  email!: string;
+
+  @Column({ type: DataType.STRING })
+  firstName!: string;
+
+  @Column({ type: DataType.STRING })
+  lastName!: string;
+
+  @Column({ type: DataType.STRING })
+  location!: string;
+
+  @Column({ type: DataType.STRING })
+  organizationName!: string;
+
+  @Column({ type: DataType.STRING })
+  phoneNumber!: string;
+
+  @Column({ type: DataType.ARRAY(DataType.STRING) })
+  preferredContactMethods!: string[];
+
+  @Column({ type: DataType.ARRAY(DataType.JSON) })
+  questions!: { answer: string; placeholder: string; question: string }[];
+
+  @Column({ type: DataType.ENUM('pending', 'accepted', 'rejected') })
+  status!: ApplicationStatus;
+
+  @Column({ type: DataType.STRING })
+  website!: string;
+
+  @Column({ type: DataType.DATE })
+  timestamp!: Date;
+}
