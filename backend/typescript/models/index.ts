1185cba794ad84388788fab03da2d42709399f4c
Simhon Chourasia <simhonchourasia@gmail.com>
2023-07-19 22:13:40 -0400

    Add necessary changes to support connecting to Heroku postgres

--- backend/typescript/models/index.ts
+++ backend/typescript/models/index.ts
@@ -10,4 +10,11 @@ const DATABASE_URL =
 /* eslint-disable-next-line import/prefer-default-export */
 export const sequelize = new Sequelize(DATABASE_URL, {
   models: [path.join(__dirname, "/*.model.ts")],
+  ssl: true,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
 });
