8e45bb930bf7fe05e48faad8bf18d6f5cdf66942
HeetShah <heetshah7642@gmail.com>
2023-10-18 20:13:04 -0400

    adds termApplications route for recent data

--- backend/typescript/server.ts
+++ backend/typescript/server.ts
@@ -7,6 +7,7 @@ import { sequelize } from "./models";
 import schema from "./graphql";
 import Application from "./models/application.model";
 
+
 const CORS_ALLOW_LIST = [
   "http://localhost:3000",
   "https://uw-blueprint-starter-code.firebaseapp.com",
@@ -62,6 +63,17 @@ admin.initializeApp({
 });
 const db = admin.database();
 const ref = db.ref("studentApplications");
+
+
+app.get("/termApplications", async (req, res) => {
+  ref.orderByChild("term").equalTo("Fall 2023").once("value", function (snapshot) {
+    const applications: Application[] = [];
+    snapshot.forEach((childSnapshot) => {
+      applications.push(childSnapshot.val());
+    });
+    res.status(200).json(applications);
+  })});
+  
 app.get("/applications", async (req, res) => {
   try {
     const snapshot = await ref.once("value");
