7e65248156b4e43c3470239b5363099edd695e5f
mxc-maggiechen <mxc.maggiechen@gmail.com>
2025-07-08 22:03:04 -0400

    cleaner code

--- backend/typescript/migrations/20250705172630-delete-bad-seeded-user-data.ts
+++ backend/typescript/migrations/20250705172630-delete-bad-seeded-user-data.ts
@@ -37,57 +37,23 @@ const PREVIOUS_USER_SEED_DATA = [
 ];
 
 export const up: Migration = async ({ context: sequelize }) => {
-  // update malformed users with id 1 and 2 with proper seeded data
-  await sequelize.getQueryInterface().bulkUpdate(
-    "users",
-    {
-      first_name: USER_SEED_DATA[0].first_name,
-      last_name: USER_SEED_DATA[0].last_name,
-      email: USER_SEED_DATA[0].email,
-      auth_id: USER_SEED_DATA[0].auth_id,
-      role: USER_SEED_DATA[0].role,
-      position: USER_SEED_DATA[0].position,
-    },
-    { id: 1 },
-  );
-
-  await sequelize.getQueryInterface().bulkUpdate(
-    "users",
-    {
-      first_name: USER_SEED_DATA[1].first_name,
-      last_name: USER_SEED_DATA[1].last_name,
-      email: USER_SEED_DATA[1].email,
-      auth_id: USER_SEED_DATA[1].auth_id,
-      role: USER_SEED_DATA[1].role,
-      position: USER_SEED_DATA[1].position,
-    },
-    { id: 2 },
-  );
+  // Update user with id 1
+  await sequelize
+    .getQueryInterface()
+    .bulkUpdate("users", USER_SEED_DATA[0], { id: 1 });
+  // Update user with id 2
+  await sequelize
+    .getQueryInterface()
+    .bulkUpdate("users", USER_SEED_DATA[1], { id: 2 });
 };
 
 export const down: Migration = async ({ context: sequelize }) => {
-  // update malformed users with id 1 and 2 with proper seeded data
-  await sequelize.getQueryInterface().bulkUpdate(
-    "users",
-    {
-      first_name: PREVIOUS_USER_SEED_DATA[1].first_name,
-      last_name: PREVIOUS_USER_SEED_DATA[1].last_name,
-      email: PREVIOUS_USER_SEED_DATA[1].email,
-      auth_id: PREVIOUS_USER_SEED_DATA[1].auth_id,
-      role: PREVIOUS_USER_SEED_DATA[1].role,
-    },
-    { id: 2 },
-  );
-
-  await sequelize.getQueryInterface().bulkUpdate(
-    "users",
-    {
-      first_name: PREVIOUS_USER_SEED_DATA[0].first_name,
-      last_name: PREVIOUS_USER_SEED_DATA[0].last_name,
-      email: PREVIOUS_USER_SEED_DATA[0].email,
-      auth_id: PREVIOUS_USER_SEED_DATA[0].auth_id,
-      role: PREVIOUS_USER_SEED_DATA[0].role,
-    },
-    { id: 1 },
-  );
+  // Revert user with id 1
+  await sequelize
+    .getQueryInterface()
+    .bulkUpdate("users", PREVIOUS_USER_SEED_DATA[0], { id: 1 });
+  // Revert user with id 2
+  await sequelize
+    .getQueryInterface()
+    .bulkUpdate("users", PREVIOUS_USER_SEED_DATA[1], { id: 2 });
 };
