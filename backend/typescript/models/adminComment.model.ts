2df8c0a020a4913e5f5d4ef5674129d6cf8fa5f0
ruiichen <frankchen99999@gmail.com>
2025-10-04 13:33:45 -0400

    - admin comment model - migration

---
+++ backend/typescript/models/adminComment.model.ts
@@ -0,0 +1,36 @@
+import {
+  Column,
+  DataType,
+  ForeignKey,
+  Model,
+  Table,
+} from "sequelize-typescript";
+import User from "./user.model";
+import ApplicantRecord from "./applicantRecord.model";
+
+@Table({ tableName: "admin_comments" })
+export default class AdminComment extends Model {
+  @Column({
+    type: DataType.UUID,
+    defaultValue: DataType.UUIDV4,
+    primaryKey: true,
+  })
+  id!: string;
+
+  @ForeignKey(() => User)
+  @Column({ type: DataType.INTEGER })
+  userId!: number;
+
+  @ForeignKey(() => ApplicantRecord)
+  @Column({ type: DataType.UUID })
+  applicantRecordId!: string;
+
+  @Column({ type: DataType.STRING, allowNull: false })
+  comment!: string;
+
+  @Column({ type: DataType.DATE, allowNull: false })
+  createdAt!: Date;
+
+  @Column({ type: DataType.DATE, allowNull: false })
+  updatedAt!: Date;
+}
