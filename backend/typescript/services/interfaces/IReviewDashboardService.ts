6a3d3bc4375076794f36a3f2b0cc17ed8557b319
ruiichen <frankchen99999@gmail.com>
2025-11-01 14:38:48 -0400

    - everything

--- backend/typescript/services/interfaces/IReviewDashboardService.ts
+++ backend/typescript/services/interfaces/IReviewDashboardService.ts
@@ -1,14 +1,16 @@
-import { ReviewDashboardRowDTO } from "../../types";
+import { ReviewDashboardRowDTO, ReviewDashboardFilter } from "../../types";
 
 interface IReviewDashboardService {
   /**
    * Pagination-supporting viewing of the virtual review dashboard
    * @Param page the page the viewer is on
    * @Param resultsPerPage the number of results per page
+   * @param filters the filters for the review dashboard results
    */
   getReviewDashboard(
     page: number,
     resultsPerPage: number,
+    filters?: ReviewDashboardFilter,
   ): Promise<ReviewDashboardRowDTO[]>;
 }
 
