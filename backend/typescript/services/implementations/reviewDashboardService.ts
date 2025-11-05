e737d78d35aa62a64dae91b1b23a1203d7376585
ruiichen <frankchen99999@gmail.com>
2025-11-04 20:59:28 -0500

    bugfix

--- backend/typescript/services/implementations/reviewDashboardService.ts
+++ backend/typescript/services/implementations/reviewDashboardService.ts
@@ -163,7 +163,7 @@ function buildWhereStatement(filter?: ReviewDashboardFilter) {
         year.push({ "$applicant.academicYear$": { [Op.regexp]: "5(A|B)" } });
       }
       if (filter.additionalFilters.includes(AdditionalFilters.SIXTH_YEAR)) {
-        year.push({ "$applicant.academicYear$": { [Op.regexp]: "6(A|B)" } });
+        year.push({ "$applicant.academicYear$": { [Op.regexp]: "(G|g)raduate" } });
       }
       if (filter.additionalFilters.includes(AdditionalFilters.IN_REVIEW)) {
         status.push({ status: { [Op.eq]: "In Review" } });
