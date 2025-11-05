0ae8ee703d5d87f0f81ef7620dc8f8dba64b604f
ruiichen <frankchen99999@gmail.com>
2025-11-04 21:20:27 -0500

    linter

--- backend/typescript/graphql/types/reviewPageType.ts
+++ backend/typescript/graphql/types/reviewPageType.ts
@@ -24,7 +24,7 @@ const reviewPageType = gql`
     timesApplied: String!
     timestamp: Int
   }
-  
+
   extend type Query {
     reviewApplicantPage(reviewedApplicantRecordId: String!): ApplicationDTO!
   }
