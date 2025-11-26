9b2b1be9f0542286d57547eb1bc3b5f8a7dcd48c
Erica Han <ericahan.38@gmail.com>
2025-11-25 21:57:14 -0500

    merge duplicate reviewed applicant record resolvers and types files

--- backend/typescript/graphql/resolvers/reviewedApplicantRecordResolvers.ts
+++ backend/typescript/graphql/resolvers/reviewedApplicantRecordResolvers.ts
@@ -1,11 +1,69 @@
 import ReviewedApplicantRecordService from "../../services/implementations/reviewedApplicantRecordService";
 import IReviewedApplicantRecordService from "../../services/interfaces/reviewedApplicantRecordService";
-import { ReviewedApplicantRecordDTO, ReviewStatus } from "../../types";
+import {
+  ReviewedApplicantRecordDTO,
+  CreateReviewedApplicantRecordDTO,
+  DeleteReviewedApplicantRecordDTO,
+  ReviewStatus
+} from "../../types";
+import { getErrorMessage } from "../../utilities/errorUtils";
 
 const reviewedApplicantRecordService: IReviewedApplicantRecordService = new ReviewedApplicantRecordService();
 
 const reviewedApplicantRecordResolvers = {
   Mutation: {
+    createReviewedApplicantRecord: async (
+      _parent: undefined,
+      args: { input: CreateReviewedApplicantRecordDTO },
+    ): Promise<ReviewedApplicantRecordDTO> => {
+      try {
+        return await reviewedApplicantRecordService.createReviewedApplicantRecord(
+          args.input,
+        );
+      } catch (error) {
+        throw new Error(getErrorMessage(error));
+      }
+    },
+
+    bulkCreateReviewedApplicantRecord: async (
+      _parent: undefined,
+      args: { inputs: CreateReviewedApplicantRecordDTO[] },
+    ): Promise<ReviewedApplicantRecordDTO[]> => {
+      try {
+        return await reviewedApplicantRecordService.bulkCreateReviewedApplicantRecord(
+          args.inputs,
+        );
+      } catch (error) {
+        throw new Error(getErrorMessage(error));
+      }
+    },
+
+    deleteReviewedApplicantRecord: async (
+      _parent: undefined,
+      args: { input: DeleteReviewedApplicantRecordDTO },
+    ): Promise<ReviewedApplicantRecordDTO> => {
+      try {
+        return await reviewedApplicantRecordService.deleteReviewedApplicantRecord(
+          args.input,
+        );
+      } catch (error) {
+        throw new Error(getErrorMessage(error));
+      }
+    },
+
+    bulkDeleteReviewedApplicantRecord: async (
+      _parent: undefined,
+      args: { inputs: DeleteReviewedApplicantRecordDTO[] },
+    ): Promise<ReviewedApplicantRecordDTO[]> => {
+      try {
+        return await reviewedApplicantRecordService.bulkDeleteReviewedApplicantRecord(
+          args.inputs,
+        );
+      } catch (error) {
+        throw new Error(getErrorMessage(error));
+      }
+    },
+    
     updateReviewStatus: async (
       _parent: undefined,
       { applicantRecordId, reviewerId, status }: { applicantRecordId: string; reviewerId: number; status: ReviewStatus },
