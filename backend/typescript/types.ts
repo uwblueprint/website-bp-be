4283d4d49ca5cf5ceec210830fbfd148a603ef23
Ethan Chen <ethansunchen@gmail.com>
2026-02-28 16:58:18 -0500

    [INTW26] Create Interview Delegations Model + Migration File (#116)
    * create interview delegations model and migration file
    
    * - make interviewHasConflict column type string instead of enum
    - capitalize interview conflict options to match constant naming
      convention

--- backend/typescript/types.ts
+++ backend/typescript/types.ts
@@ -303,3 +303,17 @@ export type CreateFirebaseFileDTO = Omit<
   FirebaseFileDTO,
   "id" | "createdAt" | "updatedAt"
 >;
+
+export const INTERVIEW_CONFLICT_OPTIONS = [
+  "APPLICANT_CONFLICT", // Conflict of interest with the applicant
+  "APPLICANT_NO_RESPONSE", // Applicant did not respond to the interview request
+  "PARTNER_NO_RESPONSE", // Partner did not respond to the interview request
+  "CANNOT_ATTEND", // Cannot make interview
+] as const;
+export type InterviewConflict = (typeof INTERVIEW_CONFLICT_OPTIONS)[number];
+
+export type InterviewDelegationDTO = {
+  interviewedApplicantRecordId: string;
+  interviewerId: number;
+  interviewHasConflict?: InterviewConflict;
+};
