a31241b204ec9300a7da1bd6b1e5f63e31b52947
Ethan Chen <ethansunchen@gmail.com>
2026-03-28 19:26:41 -0400

    [INTW26] Build CRUD Endpoints for Interview Delegations (#120)
    * create interface for interview delegations service
    
    * implement interview delegations crud service
    
    * add resolvers for interview delegations service methods
    
    * define graphql types required for interview delegations queries and
    mutations
    
    * register interview delegations graphql resources
    
    * add createdAt and updatedAt columns to interview delegations
    
    * rename InterviewDelegationDTO interviewGroupId to groupId for
    consistency with db, update toInterviewDelegationDTO to include groupId
    
    * add groupId params to create and update interview delegation endpoints
    
    * change DeleteInterviewDelegationsDTO to only consist of the pks
    
    * add migration file for setting interviewGroup schedulingLink to nullable
    
    * add groupId to InterviewDelegation graphql type

---
+++ backend/typescript/migrations/20260302002547-add-interview-delegation-timestamp-columns.ts
@@ -0,0 +1,23 @@
+import { DataType } from "sequelize-typescript";
+import { Migration } from "../umzug";
+
+const TABLE_NAME = "interview_delegations";
+
+export const up: Migration = async ({ context: sequelize }) => {
+  await sequelize.getQueryInterface().addColumn(TABLE_NAME, "createdAt", {
+    type: DataType.DATE,
+    allowNull: false,
+    defaultValue: DataType.NOW,
+  });
+
+  await sequelize.getQueryInterface().addColumn(TABLE_NAME, "updatedAt", {
+    type: DataType.DATE,
+    allowNull: false,
+    defaultValue: DataType.NOW,
+  });
+};
+
+export const down: Migration = async ({ context: sequelize }) => {
+  await sequelize.getQueryInterface().removeColumn(TABLE_NAME, "createdAt");
+  await sequelize.getQueryInterface().removeColumn(TABLE_NAME, "updatedAt");
+};
